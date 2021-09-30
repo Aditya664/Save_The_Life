2735dc379f81200a1714df6dc2661e065e786824
Aditya <Adityadeshmukh7350@gmail.com>
2021-09-30 09:27:10 -0700

    Created New Project

---
+++ app/src/main/java/com/example/savethelife/Activity/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.savethelife.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.window.SplashScreen;
+
+import com.example.savethelife.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ app/src/main/java/com/example/savethelife/Activity/SpalshScreen.java
@@ -0,0 +1,27 @@
+package com.example.savethelife.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.window.SplashScreen;
+
+import com.example.savethelife.R;
+
+public class SpalshScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh_screen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SpalshScreen.this,MainActivity.class));
+                finish();
+            }
+        },2500);
+    }
+}
