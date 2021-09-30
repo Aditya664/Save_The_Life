21d24e6e30de432c1de0e0a31c70b81f36c6d393
Aditya <Adityadeshmukh7350@gmail.com>
2021-09-30 13:48:53 -0700

    new

--- app/src/main/java/com/example/savethelife/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/savethelife/Activity/RegisterActivity.java
@@ -1,16 +1,14 @@
 package com.example.savethelife.Activity;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceManager;
-
 import android.content.Intent;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.preference.PreferenceManager;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Request.Method;
@@ -18,9 +16,10 @@ import com.android.volley.Response.ErrorListener;
 import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.example.savethelife.R;
+import com.example.savethelife.Utils.EndPoints;
 import com.example.savethelife.Utils.VolleySingleton;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -28,96 +27,103 @@ import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText Name,City,Bloodgrp,Pass,mbl;
-    private Button subBtn;
+    private EditText nameEt, cityEt, bloodGroupEt, passwordEt, mobileEt;
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Name = findViewById(R.id.name);
-        City = findViewById(R.id.city);
-        Bloodgrp = findViewById(R.id.blood_group);
-        Pass = findViewById(R.id.password);
-        mbl = findViewById(R.id.number);
-        subBtn = findViewById(R.id.reg_btn);
-        subBtn.setOnClickListener(new View.OnClickListener() {
+        nameEt = findViewById(R.id.name);
+        cityEt = findViewById(R.id.city);
+        bloodGroupEt = findViewById(R.id.blood_group);
+        passwordEt = findViewById(R.id.password);
+        mobileEt = findViewById(R.id.number);
+        submitButton = findViewById(R.id.reg_btn);
+        submitButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name,city,blood_group,password,number;
-                name = Name.getText().toString();
-                city = City.getText().toString();
-                blood_group = Bloodgrp.getText().toString();
-                password = Pass.getText().toString();
-                number = mbl.getText().toString();
-                if(isValid(name,city,blood_group,password,number)){
-                    register(name,city,blood_group,password,number);
+                String name, city, blood_group, password, mobile;
+                name = nameEt.getText().toString();
+                city = cityEt.getText().toString();
+                blood_group = bloodGroupEt.getText().toString();
+                password = passwordEt.getText().toString();
+                mobile = mobileEt.getText().toString();
+                if (isValid(name, city, blood_group, password, mobile)) {
+                    register(name, city, blood_group, password, mobile);
                 }
             }
         });
+
     }
-    private void register(final String name,final String  city,final String blood_group,final String password,final String number){
-       StringRequest stringRequest = new StringRequest(Method.POST, EndPoints.register_url, new Listener<String>() {
 
-           @Override
-           public void onResponse(String response) {
-               if(response.equals("Success")){
-                   PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit()
-                           .putString("city", city).apply();
-                   Toast.makeText(RegisterActivity.this, response, Toast.LENGTH_SHORT).show();
-                   startActivity(new Intent(RegisterActivity.this, MainActivity.class));
-                   RegisterActivity.this.finish();
-               }else{
-                   Toast.makeText(RegisterActivity.this, response, Toast.LENGTH_SHORT).show();
-               }
-           }
-       }, new ErrorListener() {
-           @Override
-           public void onErrorResponse(VolleyError error) {
-               Toast.makeText(RegisterActivity.this, "Something went wrong:(", Toast.LENGTH_SHORT).show();
-               Log.d("VOLLEY",error.getMessage());
-           }
-       }){
-           protected Map<String,String> getParams() throws AuthFailureError {
-               Map<String,String> params = new HashMap<>();
-               params.put("name",name);
-               params.put("city",city);
-               params.put("blood_group",blood_group);
-               params.put("password",password);
-               params.put("number",number);
 
-               return params;
-           }
-       };
+    private void register(final String name, final String city, final String blood_group, final String password,
+                          final String mobile) {
+        StringRequest stringRequest = new StringRequest(Method.POST, EndPoints.register_url, new Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(response.equals("Success")){
+                    Toast.makeText(RegisterActivity.this, response, Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                    RegisterActivity.this.finish();
+                }else{
+                    Toast.makeText(RegisterActivity.this, response, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(RegisterActivity.this, "Something went wrong:(", Toast.LENGTH_SHORT).show();
+                Log.d("VOLLEY", error.getMessage());
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("name", name);
+                params.put("city", city);
+                params.put("blood_group", blood_group);
+                params.put("password", password);
+                params.put("number", mobile);
+                return params;
+            }
+        };
         VolleySingleton.getInstance(this).addToRequestQueue(stringRequest);
-
     }
-        private boolean isValid(String name, String city, String blood_group, String number, String password){
-        List<String> valid_blood_group = new ArrayList<>();
-        valid_blood_group.add("A+");
-        valid_blood_group.add("A-");
-        valid_blood_group.add("B+");
-        valid_blood_group.add("B-");
-        valid_blood_group.add("AB+");
-        valid_blood_group.add("AB-");
-        valid_blood_group.add("O+");
-        if(name.isEmpty()){
+
+
+    private boolean isValid(String name, String city, String blood_group, String password,
+                            String mobile) {
+        List<String> valid_blood_groups = new ArrayList<>();
+        valid_blood_groups.add("A+");
+        valid_blood_groups.add("A-");
+        valid_blood_groups.add("B+");
+        valid_blood_groups.add("B-");
+        valid_blood_groups.add("AB+");
+        valid_blood_groups.add("AB-");
+        valid_blood_groups.add("O+");
+        valid_blood_groups.add("O-");
+        if (name.isEmpty()) {
             showMessage("Name is empty");
             return false;
-        }else if(city.isEmpty()){
-            showMessage("City is empty");
+        } else if (city.isEmpty()) {
+            showMessage("City name is required");
             return false;
-        }else if(!valid_blood_group.contains(blood_group)){
-            showMessage(" Blood group Choose from "+valid_blood_group);
+        } else if (!valid_blood_groups.contains(blood_group)) {
+            showMessage("Blood group invalid choose from " + valid_blood_groups);
             return false;
-        }else if(number.length() != 10){
-            showMessage("number is invalid");
+        } else if (mobile.length() != 10) {
+            showMessage("Invalid mobile number, number should be of 10 digits");
             return false;
         }
         return true;
+    }
 
+
+    private void showMessage(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
-    private void showMessage(String msg){
-        Toast.makeText(this, msg , Toast.LENGTH_SHORT).show();
-    }
+
+
 }
